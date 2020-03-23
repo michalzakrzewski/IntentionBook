da0416b854bf46303e41e49a57b5c4e875093013
Michał Zakrzewski <michal.zakrzewsky@gmail.com>
2020-03-23 13:49:34 +0100

    {IB-15}Usunięcie importów

--- src/main/java/com/zakrzewski/intentionbook/controllers/BookOfIntentionController.java
+++ src/main/java/com/zakrzewski/intentionbook/controllers/BookOfIntentionController.java
@@ -10,8 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @Controller
